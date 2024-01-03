561e4efe25fd3e6368b94df986366980d6f30345
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-01-03 04:04:31 -0500

    increased paddle speed. todo: improve paddle movement

--- src/main/java/xd/arkosammy/breakout/sprite/Paddle.java
+++ src/main/java/xd/arkosammy/breakout/sprite/Paddle.java
@@ -15,9 +15,9 @@ public class Paddle extends AbstractSprite {
 
         double[] newCoordinate;
         if(direction == Direction.LEFT){
-            newCoordinate = new double[]{this.coordinate[0] - 1, this.coordinate[1]};
+            newCoordinate = new double[]{this.coordinate[0] - 2, this.coordinate[1]};
         } else {
-            newCoordinate = new double[]{this.coordinate[0] + 1, this.coordinate[1]};
+            newCoordinate = new double[]{this.coordinate[0] + 2, this.coordinate[1]};
         }
 
         for(int i = (int) Math.round(newCoordinate[0]); i < Math.round(newCoordinate[0]) + dimensions[0]; i++){
